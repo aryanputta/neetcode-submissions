d416d0ea89abbd26669ab1eea858f925ba24d921
aryan <aryansputta@gmail.com>
2026-07-14 15:33:35 -0400

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = {}
+        # Count frequencies
+        for num in nums:
+            if num in freq:
+                freq[num] += 1
+            else:
+                freq[num] = 1
+        # Sort the numbers based on their frequency
+        sorted_freq = sorted(freq,  ##take nums inside hashmap and sort them 
+            key=freq.get,  ## look up freq in hashmap and compare those
+            reverse=True) ## want to flip order 
+        # Return the first k numbers
+        return sorted_freq[:k]
